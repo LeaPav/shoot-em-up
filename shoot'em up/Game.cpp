3c3c6ad67bbc9c17350c2a920a8bc29767c1f9f8
lpavel <lpavel@gaming.tech>
2024-12-03 12:04:18 +0100

    création de la fenetre de jeu

--- shoot'em up/Game.cpp
+++ shoot'em up/Game.cpp
@@ -1,1 +1,53 @@
 #include "Game.h"
+
+int Game::initSprite()
+{
+	if (!this->texture.loadFromFile("test.png"));
+	return -1;
+}
+
+void Game::initTexture()
+{
+	this->spriteMap.setTexture(this->texture);
+	//this->spriteMap.setPosition(0, 0);
+}
+
+void Game::initWindow()
+{
+	this->videoMode.width = mapWidth;
+	this->videoMode.height = mapHeight;
+	this->window = new RenderWindow(this->videoMode, "StarWater");
+}
+
+Game::Game()
+{
+	//this->initSprite();
+	//this->initTexture();
+	this->initWindow();
+}
+
+Game::~Game()
+{
+
+}
+
+const bool Game::windowIsOpen()
+{
+	return this->window->isOpen();
+}
+
+void Game::update()
+{
+	Event event;
+	while (this->window->pollEvent(event)) {
+		if (event.type == Event::Closed)
+			this->window-> close();
+	}
+}
+
+void Game::render()
+{
+	this->window->clear();
+	this->window ->display();
+
+}
--- shoot'em up/Game.h
+++ shoot'em up/Game.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "stdafx.h"
 
 class Game
 {
+private:
+	RenderWindow* window;
+	VideoMode videoMode;
+	//fonctions
+	int initSprite();
+	void initTexture();
+	void initWindow();
+	//taille de l'écran
+	int mapWidth = 1920;
+	int mapHeight = 1080;
+	Sprite spriteMap;
+	Texture texture;
+public:
+	Game();
+	~Game();
+
+	const bool windowIsOpen();
+	void update();
+	void render();
 
 };
 
