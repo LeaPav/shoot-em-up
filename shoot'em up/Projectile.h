9f62402c348ff025ed80c5897af29da4d5df8613
Lea <lea@DESKTOP-05TVQDE>
2024-12-07 16:11:04 +0100

    modif projectile

--- shoot'em up/Projectile.h
+++ shoot'em up/Projectile.h
@@ -15,7 +15,6 @@ public:
 
 	void update();
 	void render(RenderTarget& target);
-	const RectangleShape& getShape() const;
 	FloatRect getGlobalBounds() const;
 	void markAsOutOfScreen();
 	bool outOfScreen() const; // avec IsOutOfScreen
