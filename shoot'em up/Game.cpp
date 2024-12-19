2342c954f4052117bf8474f2d8b8bfc035371325
Kpommez <kpommez@gaming.tech>
2024-12-19 22:47:34 +0100

    ennemies annulé

--- shoot'em up/Game.cpp
+++ shoot'em up/Game.cpp
@@ -1071,22 +1071,6 @@ void Game::createEnnemy() //créateur des ennemies + vagues
 
 	if (scoreBoss < spawnBoss && vagueActif && lore == false) {
 
-		bool grp1 = true;
-		bool grp2 = true;
-		bool grp3 = true;
-		bool grp4 = true;
-
-		if (random == 3 && grp4 == false) {
-			random = random - 1;
-		}
-
-		if (random == 2 && grp3 == false) {
-			random = random - 1;
-		}
-
-		if (random == 1 && grp3 == true) {
-			random = random - 1;
-		}
 
 		while (random == 3 && scoreBoss < 20) {
 			random = rand() % 3;
@@ -1096,7 +1080,7 @@ void Game::createEnnemy() //créateur des ennemies + vagues
 		}
 		
 
-		if (random == 0 && grp1 == true) {   //pyramide par 3 tir
+		if (random == 0) {   //pyramide par 3 tir
 
 			MovementType randomType = static_cast<MovementType>(rand() % 2);
 
@@ -1127,7 +1111,7 @@ void Game::createEnnemy() //créateur des ennemies + vagues
 			}
 			ennemies.push_back(newEnnemy3);
 		}
-		if (random == 1 && grp2 == true) { //mur par 3 passif
+		if (random == 1) { //mur par 3 passif
 
 			MovementType randomType = static_cast<MovementType>(rand() % 4);
 
@@ -1154,7 +1138,7 @@ void Game::createEnnemy() //créateur des ennemies + vagues
 			}
 			ennemies.push_back(newEnnemy3);
 		}
-		if (random == 2 && scoreBoss >= 40 && grp3 == true || random == 2 && grp1 ==false && grp2==false) { // pyramide par 5 tir
+		if (random == 2 && scoreBoss >= 40) { // pyramide par 5 tir
 
 				int largeur = rand() % this->videoMode.height;
 
@@ -1196,7 +1180,7 @@ void Game::createEnnemy() //créateur des ennemies + vagues
 
 
 		}
-		if (random == 3 && scoreBoss >= 20 && grp4 == true || random == 3 && grp1 == false && grp2 == false) { //mur par 5
+		if (random == 3 && scoreBoss >= 20) { //mur par 5
 
 				MovementType randomType = static_cast<MovementType>(rand() % 4);
 
